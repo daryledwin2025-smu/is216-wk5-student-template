c88fe3210ae122810f34346ea0b7d0e456503661
daryledwin2025-smu <daryl.edwin.2025@smu.edu.sg>
2026-09-14 09:37:27 +0800

    keydown fix

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -25,5 +25,5 @@ function addItem(event) {
   </ul>
 
     <input v-model="newItem">
-    <button @click="addItem" type="submit" @keyup="addItem">Add!</button>
+    <button @click="addItem" type="submit" @keydown.enter="addItem">Add!</button>
 </template>
