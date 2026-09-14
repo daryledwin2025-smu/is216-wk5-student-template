7f9b79c54a85afed6b3eae4a0d5100e544d46d05
daryledwin2025-smu <daryl.edwin.2025@smu.edu.sg>
2026-09-14 09:28:46 +0800

    ex 4 fix :O

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -1,23 +1,31 @@
 <script setup>
 import { ref } from 'vue'
+
 const items = ref(['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'])
+const newItem = ref('')
+
 function deleteItem(index) {
-  items.value.pop(index);
+  items.value.splice(index, 1)
+}
+
+function addItem() {
+  items.value.push(newItem.value)
+  newItem.value = ''
 }
 </script>
 
 <template>
   <h2>Shopping Cart</h2>
-   <form action="">
-    <ul>
-      <div v-for="(value, index) in items" :key="value">
-    <li>{{ value }}</li>
-    <button type="button" @click="deleteItem(index)">Delete</button>
-      </div>
 
-    </ul>
-   </form>
-   <div v-for="value in items">
-    {{ value }}
-   </div>
+  <ul>
+    <li v-for="(value, index) in items" :key="value">
+      {{ value }}
+      <button type="button" @click="deleteItem(index)">Delete!</button>
+    </li>
+  </ul>
+
+  <form @submit.prevent="addItem">
+    <input v-model="newItem">
+    <button type="submit">Add!</button>
+  </form>
 </template>
