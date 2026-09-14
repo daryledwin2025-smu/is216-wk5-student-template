5fcca1d1f77f978b9bed0b413019eb04861e5020
daryledwin2025-smu <daryl.edwin.2025@smu.edu.sg>
2026-09-14 08:56:16 +0800

    ex1-3

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -1,11 +1,23 @@
 <script setup>
 import { ref } from 'vue'
 const items = ref(['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'])
-
-// TODO: Add Code Here
+function deleteItem(index) {
+  items.value.pop(index);
+}
 </script>
 
 <template>
   <h2>Shopping Cart</h2>
-  <!-- TODO: Add Code Here -->
+   <form action="">
+    <ul>
+      <div v-for="(value, index) in items" :key="value">
+    <li>{{ value }}</li>
+    <button type="button" @click="deleteItem(index)">Delete</button>
+      </div>
+
+    </ul>
+   </form>
+   <div v-for="value in items">
+    {{ value }}
+   </div>
 </template>
